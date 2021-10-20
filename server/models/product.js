f686e9420da2ae4424e472b830d2c56d53e43682
tobiasz18 <tobiaszpost@gmail.com>
2021-10-20 23:34:25 +0200

    Create product schema

---
+++ server/models/product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+const { ObjectId } = mongoose.Schema
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 2000
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 2000
+  },
+  price: {
+    type: Number,
+    trim: true,
+    required: true,
+    maxlength: 32
+  },
+  category: {
+    type: ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+  },
+  photo: {
+    data: Buffer,
+    contentType: String,
+  },
+  shipping: {
+    required: false,
+    type: Boolean,
+  }
+}, { timestamps: true })
+
+const Category = mongoose.model("Product", productSchema)
+
+module.exports = { Category }
