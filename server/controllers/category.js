a7c871882b51c76e5ddab8f04f63c002b8d4e75f
tobiasz18 <tobiaszpost@gmail.com>
2021-10-26 21:37:43 +0200

    Create find category by id endpoint

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,6 +1,20 @@
 const { Category } = require("../models/category")
 const { errorHandler } = require("../helpers/dbErrorHandler")
 
+/**** param ****/
+exports.categoryById = async (req, res, next, id) => {
+  try {
+    const category = await Category.findById(id).exec()
+    req.category = category
+    next()
+    
+  } catch (error) {
+    return res.status(400).json({
+      error: `Category not found`
+    })
+  }
+}
+
 /**** endpoints ****/
 exports.create = (req, res) => {
   const category = new Category(req.body)
@@ -12,4 +26,8 @@ exports.create = (req, res) => {
     }
     res.json({ data })
   })
+}
+
+exports.read = (req, res) => {
+  return res.json(req.category)
 }
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { create } = require('../controllers/category')
+const { create, read, categoryById } = require('../controllers/category')
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth")
 const { userById } = require('../controllers/user')
 
-router.param('userId', userById) // set req.profile = user
+router.param('userId', userById) // set req.profile = user  by id
+router.param('categoryId', categoryById) // set req.category = category  by id
 
+router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create)
 
 module.exports = router
