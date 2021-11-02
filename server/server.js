0a590669d112112a2130533932e1795dc6ed41b5
tobiasz18 <tobiaszpost@gmail.com>
2021-11-02 20:56:41 +0100

    Add cors middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const expressValidator = require('express-validator');
 require('dotenv').config()
 
@@ -23,6 +24,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(expressValidator())
+app.use(cors())
 
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
