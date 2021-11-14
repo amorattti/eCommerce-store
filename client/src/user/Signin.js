c247456f59953a0cc572b5c702a66db2d81a2e4e
tobiasz18 <tobiaszpost@gmail.com>
2021-11-14 22:12:47 +0100

    sign in styles

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -3,6 +3,12 @@ import Layout from '../hoc/Layout'
 import { Link } from 'react-router-dom'
 import { signin } from '../auth'
 
+import { EntryPage, PageHeader } from './style'
+import EntryCard from '../components/EntryCard'
+import InputGroup from '../components/InputGroup'
+import Input from '../components/Input'
+import Button from '../components/Button'
+
 const Signin = () => {
   const [values, setValues] = useState({
     email: '',
@@ -38,28 +44,50 @@ const Signin = () => {
   }
 
   const signUpForm = () => (
-    <form>
-      <div className="form-group">
-        <label className="text-muted">Email</label>
-        <input
-          onChange={handleChange('email')}
-          type="email"
-          className="form-control"
-          value={email}
-        />
-      </div>
+    // <form>
+    //   <div className="form-group">
+    //     <label className="text-muted">Email</label>
+    //     <input
+    //       onChange={handleChange('email')}
+    //       type="email"
+    //       className="form-control"
+    //       value={email}
+    //     />
+    //   </div>
 
-      <div className="form-group">
-        <label className="text-muted">Password</label>
-        <input
-          onChange={handleChange('password')}
-          type="password"
-          className="form-control"
-          value={password}
-        />
-      </div>
-      <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
-    </form>
+    //   <div className="form-group">
+    //     <label className="text-muted">Password</label>
+    //     <input
+    //       onChange={handleChange('password')}
+    //       type="password"
+    //       className="form-control"
+    //       value={password}
+    //     />
+    //   </div>
+    //   <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+    // </form>
+    <EntryPage>
+      <EntryCard>
+        <h2>
+          Sign in
+        </h2>
+        <form>
+          <InputGroup>
+            <label htmlFor="login-email" placeholder="email">Email Address</label>
+            <Input type="text" />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor="login-password" placeholder="password">Password</label>
+            <Input type="text" />
+          </InputGroup>
+          <Button type="submit" full>Sign in</Button>
+          <span>
+            Don't have an account?
+            <Link to="/signup">Sign up</Link>
+          </span>
+        </form>
+      </EntryCard>
+    </EntryPage>
   )
 
   const showError = () => (
