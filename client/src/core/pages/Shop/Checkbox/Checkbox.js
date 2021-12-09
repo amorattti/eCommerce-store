f02e0bdabf8c7490313d2fadd770c6dc9ef7859d
tobiasz18 <tobiaszpost@gmail.com>
2021-12-09 15:35:48 +0100

    add chexbox

--- client/src/core/pages/Shop/Checkbox/Checkbox.js
+++ client/src/core/pages/Shop/Checkbox/Checkbox.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Checkbox = ({ categories }) => {
+  const [checked, setChecked] = useState([])
+
+  const handleToggle = (categoryId) => {
+    // check if we have our category in checked array
+    // and return index or -1
+    const currentIndexCategory = checked.indexOf(categoryId)
+    const newArrayOfCategories = [...checked]
+    
+    if(currentIndexCategory === -1) {
+      newArrayOfCategories.push(categoryId)
+    } else {
+      newArrayOfCategories.splice(currentIndexCategory, 1)
+    }
+
+    setChecked(newArrayOfCategories)
+  }
+
   return categories.map((category, i) => (
     <li key={i}>
-      <input type="checkbox"/>
+      <input
+        type="checkbox"
+        value={checked.indexOf(category._id === -1)}
+        onChange={() => handleToggle(category._id)}
+      />
       <label>{category.name}</label>
     </li>
   ))
