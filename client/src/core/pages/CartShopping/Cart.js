64dfab9215ec44c1fe41103ee95dbc059f97efb7
tobiasz18 <tobiaszpost@gmail.com>
2021-12-29 23:45:50 +0100

    show items

--- client/src/core/pages/CartShopping/Cart.js
+++ client/src/core/pages/CartShopping/Cart.js
@@ -3,57 +3,58 @@ import { Link } from 'react-router-dom'
 import Layout from '../../../hoc/Layout'
 import { getCart } from '../../cartHelper'
 import Card from '../../../components/Card'
-import { Row, Col } from './style'
+import { Row, Col, Table } from './style'
 import ShowImage from '../../../components/ShowImage'
+import * as S from './style'
 
 const Cart = () => {
   const [items, setItems] = useState([])
 
   useEffect(() => {
-
-    setItems(getCart())
+    if (getCart() !== null) {
+      setItems(getCart())
+    }
   }, [])
 
 
-
+  console.log('items', '', items)
 
   const showItems = items => {
     return (
-      <div>
-        <h2>
-          Your cart has {`${items.length}`} items
-        </h2>
-        <hr />
-        <table>
-          {items.map((product) => (
-            <tr key={product._id}>
-              <td>
-                <ShowImage height="120px" width='120px' url="product" item={product}/>
-              </td>
-              <td>{product.name}</td>
-              <td>
-                <div>
-                  <span>+</span>
-                  <span>{product.quantity}</span>
-                  <span>-</span>
-                </div>
-              </td>
-              <td>{product.price}</td>
-              <td>
-                <button>Remove</button>
-              </td>
-            </tr>
-          ))}
-        </table>
-      </div>
+      <S.ShoppingCart>
+        <S.TitleCart>Shopping Bag</S.TitleCart>
+        <S.ItemBody>
+
+          <S.Image>
+            <img src="" alt="" />
+          </S.Image>
+
+          <S.Name>
+            <span>book</span>
+          </S.Name>
+
+          <S.Quantity>
+            <button>
+              <img src="" alt="s" /> +
+            </button>
+            <input type="text" name="name" value="1"></input>
+            <button>
+              <img src="" alt="" /> -
+            </button>
+          </S.Quantity>
+
+          <S.TotalPrice>$549</S.TotalPrice>
+
+        </S.ItemBody>
+      </S.ShoppingCart>
     )
   }
 
-  const noItemsMessage = () => {
+  const noItemsMessage = () => (
     <h2>
       Your cart is empty. <br /> <Link to="/shop">Continue shopping</Link>
     </h2>
-  }
+  )
 
   return (
     <Layout>
@@ -61,7 +62,9 @@ const Cart = () => {
         <Col size={8}>
           {items.length > 0 ? showItems(items) : noItemsMessage()}
         </Col>
-        <Col ize={4}>
+        <Col size={4} style={{ border: '1px solid gray' }}>
+          <h2>Summary</h2>
+          <hr />
           <p>show checkout options/shipping address/total/update quantity</p>
         </Col>
       </Row>
--- client/src/core/pages/CartShopping/style.js
+++ client/src/core/pages/CartShopping/style.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+export const Grid = styled.div`
+
+`
+
 export const Row = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -9,3 +13,44 @@ export const Col = styled.div`
   flex: ${(props) => props.size};
 `
 
+//     inline-size: 150px;
+// overflow-wrap: break-word;
+
+
+
+export const ShoppingCart = styled.div`
+
+`
+export const TitleCart = styled.div`
+
+`
+
+export const ItemBody = styled.div`
+
+
+`
+export const Image = styled.div`
+
+
+`
+export const Name = styled.div`
+
+
+`
+export const Quantity = styled.div`
+
+input {
+  -webkit-appearance: none;
+  border: none;
+  text-align: center;
+  width: 32px;
+  font-size: 16px;
+  color: #43484D;
+  font-weight: 300;
+}
+
+`
+export const TotalPrice = styled.div`
+
+
+`
