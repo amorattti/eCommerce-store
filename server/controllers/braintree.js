6e73f85e34c9beaa25036f00b73b4ba2eec3c169
tobiasz18 <tobiaszpost@gmail.com>
2022-01-06 23:05:58 +0100

    braintree proccessing payment backend

--- server/controllers/braintree.js
+++ server/controllers/braintree.js
@@ -3,18 +3,40 @@ const braintree = require('braintree')
 require('dotenv').config()
 
 const gateway = new braintree.BraintreeGateway({
-  environment:  braintree.Environment.Sandbox,
-  merchantId:   process.env.BRAINTREE_MARCHANT_ID,
-  publicKey:    process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey:   process.env.BRAINTREE_PRIVATE_KEY
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MARCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY
 });
 
 exports.generateToken = (req, res) => {
-  gateway.clientToken.generate({}, function(err, response) {
-    if(err) {
+  gateway.clientToken.generate({}, function (err, response) {
+    if (err) {
       res.status(500).send(err)
     } else {
       res.send(response)
     }
   })
+}
+
+exports.processPayment = (req, res) => {
+  const paymentMethodNonce = req.body.paymentMethodNonce
+  const amount = req.body.amount
+
+  gateway.transaction.sale(
+    {
+      amount: amount,
+      paymentMethodNonce: paymentMethodNonce,
+      options: {
+        submitForSettlement: true
+      }
+    },
+    (error, result) => {
+      if (error) {
+        res.status(500).json(error)
+      } else {
+        res.json(result)
+      }
+    }
+  )
 }
--- server/routes/braintree.js
+++ server/routes/braintree.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const { requireSignin, isAuth } = require('../controllers/auth')
-const { generateToken } = require('../controllers/braintree')
+const { generateToken, processPayment } = require('../controllers/braintree')
 const { userById } = require('../controllers/user')
 const router = express.Router()
 
 router.param('userId', userById)
 
-
 router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken)
-
-
+router.post('/braintree/payment/:userId', requireSignin, isAuth, processPayment)
 
 module.exports = router
