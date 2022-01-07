bdf0c8c8c24cd35b647b5a7732b4b949c2a05d6b
tobiasz18 <tobiaszpost@gmail.com>
2022-01-07 17:17:37 +0100

    update title css

--- client/src/components/Grid/style.js
+++ client/src/components/Grid/style.js
@@ -4,8 +4,8 @@ export const StyledGrid = styled.div`
   display: grid;
   grid-template-columns: ${props => props.template ? props.template : '20% 20% 20% 20% 20%'};
   gap: ${props => props.gap ? props.gap : 'auto'};
-  padding-bottom: 20px;
-  margin-bottom: 50px;
+  padding: 30px;
+  margin-bottom: 100px;
 
 @media(max-width: 768px) {
   grid-template-columns: 1fr 1fr;
--- client/src/core/pages/Home/style.js
+++ client/src/core/pages/Home/style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
 export const Header = styled.h2` 
-    font-family: 'Merriweather';
-    margin-bottom: 32px;
+   
+    
 `
