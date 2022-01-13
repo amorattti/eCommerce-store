031bd543ea99029635e365d72ea7994d3875b762
tobiasz18 <tobiaszpost@gmail.com>
2022-01-13 22:20:49 +0100

    Add resp Table to order page

---
+++ client/src/admin/Orders/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table';
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+
+
+export const TableStyled = styled(Table)`
+
+`
+
+export const THeadStyled = styled(Thead)`
+
+`
+
+export const TBodyStyled = styled(Tbody)`
+
+`
+
+export const TRStyled = styled(Tr)`
+
+`
+
+export const THStyled = styled(Th)`
+
+`
+export const TDstyled = styled(Td)`
+
+`
