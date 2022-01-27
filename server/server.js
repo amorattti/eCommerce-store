648d42d1801a59992a21ceb7ff971880f02e5ccf
tobiasz18 <tobiaszpost@gmail.com>
2022-01-27 23:31:03 +0100

    heroku

--- server/server.js
+++ server/server.js
@@ -35,6 +35,17 @@ app.use('/api', productRoutes)
 app.use('/api', braintreeRoutes)
 app.use('/api', orderRoutes)
 
+/**
+ * Hosting 
+ */
+ app.use(express.static('client/build'));
+
+ if (process.env.NODE_ENV === 'production') {
+   const path = require('path');
+   app.get('/*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, '../client','build','index.html'))
+   });
+ }
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
