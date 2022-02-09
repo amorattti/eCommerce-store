6c0edbbfd2f0b39d6ab85dcf5a44ad7e7179a6b9
tobiasz18 <tobiaszpost@gmail.com>
2022-02-09 23:58:56 +0100

    Menu filters sticky rwd bar

--- client/src/core/pages/Shop/Shop.js
+++ client/src/core/pages/Shop/Shop.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { getCategories, getFilteredProducts } from '../../apiCore'
 import { prices } from '../../fixedPrices'
-
 import Layout from '../../../hoc/Layout'
-import { ButtonLoadMore, Col, Row } from './style'
 import Checkbox from '../../../components/Checkbox'
 import RadioBox from '../../../components/RadioBox'
 import Card from '../../../components/Card/Card'
 import Grid from '../../../components/Grid'
+import { IoFilterSharp } from 'react-icons/all'
+import { BsArrowLeftShort } from 'react-icons/bs'
+import {MdOutlineReadMore} from 'react-icons/all'
+
+import {
+  ButtonLoadMore,
+  Row,
+  FiltersHeader,
+  Carts, MenuBar,
+  ContentBar,
+  ToggleFilter
+} from './style'
 
 const Shop = ({ searchValue, setSearchValue }) => {
   const [myFilters, setMyFilters] = useState({
@@ -19,6 +29,8 @@ const Shop = ({ searchValue, setSearchValue }) => {
   const [skip, setSkip] = useState(0)
   const [size, setSize] = useState(0)
   const [filteredResults, setFilteredResults] = useState([])
+
+  const [showBar, setShowBar] = useState(true)
 
   const init = async () => {
     const response = await getCategories()
@@ -74,7 +86,7 @@ const Shop = ({ searchValue, setSearchValue }) => {
 
   const laodMore = () => {
     const setPage = skip + limit
-    
+
     getFilteredProducts(setPage, limit, myFilters.filters).then(data => {
       if (data.error) {
         setError(data.error)
@@ -85,45 +97,70 @@ const Shop = ({ searchValue, setSearchValue }) => {
       }
     })
   }
-  
+
+  console.log(showBar)
+
   return (
     <Layout
       title="Shop page"
       description="search and find your favorite books"
     >
       <Row>
-        <Col size={2}>
-          <ul>
-            <h5>Filter by category</h5>
-            <Checkbox
-              categories={categories}
-              handleFilters={(filters) => handleFilters(filters, 'category')}
-            />
-          </ul>
-          <ul>
-            <h5>Filter by Price</h5>
-            <RadioBox
-              prices={prices}
-              handleFilters={(filters) => handleFilters(filters, 'price')}
-            />
-          </ul>
-        </Col>
-        <Col size={10}>
-          <Grid template="1fr 1fr 1fr 1fr" gap="40px 0%">
-            {filteredResults.length !== 0 ? filteredResults.map(product => (
-              <Card key={product._id} product={product} />
-            )) : <div style={{ textAlign: 'left', width: '200%' }}>
-              <h5>No products found!</h5>
-              <p>Please change Your search criteria and try again.
-                If still not finding anything relevant,
-                please visit home page and try out some of our bestsellers!
-              </p>
-            </div>}
-          </Grid>
+        <MenuBar showBar={showBar}>
+          <ToggleFilter
+            title="Filters"
+            showBar={showBar}
+            onClick={() => setShowBar(prevState => !prevState)}
+          >
+            <div>
+              <MdOutlineReadMore size='2rem' />
+            </div>
+          </ToggleFilter>
+          <FiltersHeader
+            showBar={showBar}
+            onClick={() => setShowBar(prevState => !prevState)}
+          >
+            <div>
+              <IoFilterSharp size='1.3rem' />
+            </div>
+            <div>Filters</div>
+            <div>
+              <BsArrowLeftShort size='1.8rem' />
+            </div>
+          </FiltersHeader>
+          <ContentBar showBar={showBar}>
+            <ul>
+              <h5>by category</h5>
+              <Checkbox
+                categories={categories}
+                handleFilters={(filters) => handleFilters(filters, 'category')}
+              />
+            </ul>
+            <ul>
+              <h5>by price</h5>
+              <RadioBox
+                prices={prices}
+                handleFilters={(filters) => handleFilters(filters, 'price')}
+              />
+            </ul>
+          </ContentBar>
+        </MenuBar>
+        <Carts>
+
+          {filteredResults.length !== 0 ? filteredResults.map(product => (
+            <Card key={product._id} product={product} />
+          )) : <div style={{ textAlign: 'left', width: '100%', marginLeft: '80px' }}>
+            <h5>No products found!</h5>
+            <p>Please change Your search criteria and try again.
+              If still not finding anything relevant,
+              please visit home page and try out some of our bestsellers!
+            </p>
+          </div>}
+
           {size > 0 && size >= limit && (
             <ButtonLoadMore onClick={() => laodMore()}>Load more...</ButtonLoadMore>
           )}
-        </Col>
+        </Carts>
       </Row>
     </Layout>
   )
--- client/src/core/pages/Shop/style.js
+++ client/src/core/pages/Shop/style.js
@@ -6,23 +6,111 @@ export const Grid = styled.div`
 
 export const Row = styled.div`
   display: flex;
+  position: relative;
 `
 
-export const Col = styled.div`
-  flex: ${(props) => props.size};
+export const MenuBar = styled.div`
+  margin-right: 20px;
+  box-shadow: 8px 0 8px -8px rgb(0 0 0 / 10%);
+  height: 100%;
+  width: ${props => props.showBar ? '230px' : '70px'};
+  position: relative;
+  background: #fafafa;
 
   ul {
     margin-bottom: 40px;
+
+    h5 {
+      font-size: 1rem;
+    }
+
+    li {
+     
+    }
 
     li > input {
       margin: 4px;
       cursor: pointer;
     }
   }
+
+  @media screen and (max-width: 1200px) {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`
+
+export const ContentBar = styled.div`
+ visibility: ${props => props.showBar ? 'unset' : 'hidden'}; 
+
+  @media screen and (max-width: 1200px) {
+    position: sticky;
+    top: 80px;
+  }
+
+`
+
+export const FiltersHeader = styled.button`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  border: none;
+  border-bottom: 1px solid #d9d5d5;
+  background: #fafafa;
+  padding: 0 25px 0 0;
+  padding-bottom: 10px;
+  visibility: ${props => props.showBar ? 'unset' : 'hidden'}; 
+
+  div:last-child {
+    margin-left: auto;
+    cursor: pointer;
+  }
+  div:nth-child(2) {
+    margin-left: 10px;
+    font-weight: 600;
+    letter-spacing: 0.6px;
+  }
+
+  @media screen and (max-width: 1200px) {
+    position: sticky;
+    top: 20px;
+  }
+`
+
+export const ToggleFilter = styled.button`
+  border: none;
+  width: 100%;
+  background: #fafafa;
+  visibility: ${props => props.showBar ? 'hidden' : 'unset'}; 
+  position: relative;
+
+  > div {
+    position: absolute;
+  }
+
+  @media screen and (max-width: 1200px) {
+    position: sticky;
+    top: 20px;
+  }
+`
+
+export const Carts = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  justify-content: center;
+  margin-bottom: 50px;
+  max-width: 940px;
+
+  @media screen and (max-width: 1200px) {
+    margin-left: 70px;
+  }
 `
 
 export const ButtonLoadMore = styled.button`
-  margin-bottom: 40px;
+  margin-top: 40px;
   background: none;
   padding: 14px 40px;
   font-weight: 600;
@@ -35,3 +123,5 @@ export const ButtonLoadMore = styled.button`
    color: #8d8d8d;
   }
 `
+
+
